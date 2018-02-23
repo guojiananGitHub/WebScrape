adf96336e3a8cb422530f58f3c099a48608661c3
guojianan <guo.j.an@163.com>
2018-02-23 16:06:40 +0800

    淘女郎爬蟲實踐，phantomJS被棄用，換谷歌瀏覽器

--- apitest.py
+++ apitest.py
@@ -6,7 +6,7 @@ proxy_info = {'host':'localhostXXX','port':xxx}
 # 為代理創建一個處理程序
 proxy_support = urllib.ProxyHandler({"http": "http://%(host)s:%(port)d" % proxy_info})
 #
-opener =
+
 
 t = Twitter(auth=OAuth("", "", "", ""))
 pythonTweets = t.search.tweets(q = "#python")
